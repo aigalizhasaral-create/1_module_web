1b052e420e187535573bd791ce4e6be3f92f6147
Davron Issakov <antius.isakov22@gmail.com>
2026-04-10 14:31:49 +0500

    V2.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG ={
+    publishLink: '1212',
+    viewLink: '1212',
+}
---
+++ js/main.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    bottonActive();
+    configEditor();
+    console.log(2);
+});
+
+function bottonActive() {
+    var currentPage = document.body.getAttribute('data-page')
+    var links = document.querySelectorAll('[data-page-link]')
+    
+    links.forEach(function (link) {
+        if (link.getAttribute('data-page-link') === currentPage) {
+            link.classList.add('active-link');
+        }
+    });
+}
+
+
+       
+function configEditor() {
+    var publishLinks = document.querySelectorAll('[data-role="publish-link"]')
+    var viewLinks = document.querySelectorAll('[data-role="view-link"]')
+
+    publishLinks.forEach(function (link) {
+        link.href= window.APP_CONFIG.publishLink;
+    });
+    viewLinks.forEach(function (link) {
+        link.href= window.APP_CONFIG.viewLink;
+    });
+}
